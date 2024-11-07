0891cc33b08c59558e4eb4f5818d27fbd57300ee
hardNormalEasyMode <petequdwns12@naver.com>
2024-11-07 23:22:28 +0900

    첫 번째, 두 번째 실습
    첫 번째, 두 번째 실습

--- bj/src/dtos/user.dto.js
+++ bj/src/dtos/user.dto.js
@@ -11,8 +11,15 @@ export const bodyToUser = (body) => {
     };
 };
 
-export const responseFromUser = (body) => {
+export const responseFromUser = ({ user, preferences }) => {
+    const preferFoods = preferences.map(
+        (preference) => preference.foodCategory.name
+    );
+
     return {
+        email: user.email,
+        name: user.name,
+        preferCategory: preferFoods,
     };
 };
 
--- bj/src/repositories/user.repository.js
+++ bj/src/repositories/user.repository.js
@@ -1,33 +1,17 @@
 import { pool } from "../db.config.js";
+import { prisma } from "../db.config.js";
 
-// User 데이터 삽입
-export const addUser = async (data) => {
-  const conn = await pool.getConnection();
+// Member 데이터 삽입
+export const addMember = async (data) => {
 
   try {
-    const [confirm] = await pool.query(
-      `SELECT EXISTS(SELECT 1 FROM member WHERE email = ?) as isExistEmail;`,
-      data.email
-    );
-
-    if (confirm[0].isExistEmail) {
+    const member = await prisma.user.findFirst({ where: { email: data.email } });
+    if (member) {
       return null;
     }
+    const created = await prisma.user.create({ data: data });
+  return created.id;
 
-    const [result] = await pool.query(
-      `INSERT INTO member (email, name, gender, address, spec_address, phone_num) VALUES (?, ?, ?, ?, ?, ?);`,
-      [
-        data.email,
-        data.name,
-        data.gender,
-        // data.birth,
-        data.address,
-        data.detailAddress,
-        data.phoneNumber,
-      ]
-    );
-
-    return result.insertId;
   } catch (err) {
     throw new Error(
       `오류가 발생했어요. 요청 파라미터를 확인해주세요. (${err})`
@@ -37,19 +21,10 @@ export const addUser = async (data) => {
   }
 };
 // 사용자 정보 얻기
-export const getUser = async (userId) => {
-  const conn = await pool.getConnection();
-
+export const getMember = async (memberId) => {
   try {
-    const [user] = await pool.query(`SELECT * FROM member WHERE id = ?;`, userId);
-
-    console.log(user);
-
-    if (user.length == 0) {
-      return null;
-    }
-
-    return user;
+    const member = await prisma.user.findFirstOrThrow({ where: { id: memberId } });
+  return member;
   } catch (err) {
     throw new Error(
       `오류가 발생했어요. 요청 파라미터를 확인해주세요. (${err})`
@@ -60,16 +35,14 @@ export const getUser = async (userId) => {
 };
 
 // 음식 선호 카테고리 매핑
-export const setPreference = async (userId, foodCategoryId) => {
-  const conn = await pool.getConnection();
-
+export const setPreference = async (memberId, foodCategoryId) => {
   try {
-    await pool.query(
-      `INSERT INTO member_prefer (category_id, member_id) VALUES (?, ?);`,
-      [foodCategoryId, userId]
-    );
-
-    return;
+    await prisma.userFavorCategory.create({
+      data: {
+        memberId: memberId,
+        foodCategoryId: foodCategoryId,
+      },
+    });
   } catch (err) {
     throw new Error(
       `오류가 발생했어요. 요청 파라미터를 확인해주세요. (${err})`
@@ -80,17 +53,19 @@ export const setPreference = async (userId, foodCategoryId) => {
 };
 
 // 사용자 선호 카테고리 반환
-export const getUserPreferencesByUserId = async (userId) => {
-  const conn = await pool.getConnection();
-
+export const getUserPreferencesByUserId = async (memberId) => {
   try {
-    const [preferences] = await pool.query(
-      "SELECT ufc.id, ufc.category_id, ufc.member_id, fcl.name " +
-        "FROM member_prefer ufc JOIN food_category fcl on ufc.category_id = fcl.id " +
-        "WHERE ufc.member_id = ? ORDER BY ufc.category_id ASC;",
-      userId
-    );
-
+    const preferences = await prisma.MemberPrefer.findMany({
+      select: {
+        id: true,
+        memberId: true,
+        foodCategoryId: true,
+        foodCategory: true,
+      },
+      where: { memberId: memberId },
+      orderBy: { foodCategoryId: "asc" },
+    });
+  
     return preferences;
   } catch (err) {
     throw new Error(
@@ -101,7 +76,7 @@ export const getUserPreferencesByUserId = async (userId) => {
   }
 };
 
-//미션 데이터 삽입
+//가게 데이터 삽입
 export const addStore = async (data) => {
   const conn = await pool.getConnection();
   console.log("body13:", data)
