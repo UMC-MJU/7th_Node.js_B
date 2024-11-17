7c2f7c536b69b712066fde7f211d6f0947f5f676
hardNormalEasyMode <petequdwns12@naver.com>
2024-11-17 22:44:39 +0900

    7 week mission
    7 week mission

--- bj/src/index.js
+++ bj/src/index.js
@@ -17,6 +17,25 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
+/**
+ * 공통 응답을 사용할 수 있는 헬퍼 함수 등록
+ */
+app.use((req, res, next) => {
+  res.success = (success) => {
+    return res.json({ resultType: "SUCCESS", error: null, success });
+  };
+
+  res.error = ({ errorCode = "unknown", reason = null, data = null }) => {
+    return res.json({
+      resultType: "FAIL",
+      error: { errorCode, reason, data },
+      success: null,
+    });
+  };
+
+  next();
+});
+
 app.use(cors()); // cors 방식 허용
 app.use(express.static("public")); // 정적 파일 접근
 app.use(express.json()); // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
@@ -52,6 +71,22 @@ app.get("/api/v1/stores/:storeId/missions", GetListStoreMissionController);
 
 // 특정 멤버의 진행 중인 미션 목록 가져오는 api
 app.get("/api/v1/members/:memberId/missions", GetListMemberMissionController);
+
+/**
+ * 전역 오류를 처리하기 위한 미들웨어
+ */
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  res.status(err.statusCode || 500).error({
+    errorCode: err.errorCode || "unknown",
+    reason: err.reason || err.message || null,
+    data: err.data || null,
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
