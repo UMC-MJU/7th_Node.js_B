0eefcb5b2987da25298c9396f6c327575c30efea
hardNormalEasyMode <petequdwns12@naver.com>
2024-12-01 16:42:21 +0900

    구글 로그인 실습 [완]

--- bj/src/index.js
+++ bj/src/index.js
@@ -13,8 +13,17 @@ import { handleUserSignUp,
   GetListMemberReviewsController,
   GetListMemberMissionController
 } from "./controllers/user.controller.js";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import session from "express-session";
+import passport from "passport";
+import { googleStrategy } from "./auth.config.js";
+import { prisma } from "./db.config.js";
 
 dotenv.config();
+
+passport.use(googleStrategy);
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
 
 const app = express();
 const port = process.env.PORT;
@@ -42,6 +51,82 @@ app.use(cors()); // cors 방식 허용
 app.use(express.static("public")); // 정적 파일 접근
 app.use(express.json()); // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
 app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형태로 본문 데이터 해석
+
+app.use(
+  session({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000, // ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
+
+// Case 1. 도메인 문제 일때
+// app.use(
+//   cors({
+//     origin: ["<프론트엔드_주소_및_포트>"],
+//   })
+// );
+
+// Case 2. 토큰 cors 오류 뜰때
+// app.use(
+//   cors({
+//     allowedHeaders: ["x-auth-token", ...],
+//   })
+// );
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/", (req, res) => {
+  console.log(req.user);
+  res.send("Hello World!");
+});
+
+// 회원 가입 api
+app.post("/api/v1/users/signup", handleUserSignUp);
+
+// 가게를 생성하는 api
+app.post("/api/v1/store", InsertStoreController);
+
+// 리뷰를 생성하는 api
+app.post("/api/v1/review", InsertReviewController);
+
+// 가게의 미션 생성하기
+app.post("/api/v1/mission", InsertMissionController);
+
+// 가게의 미션을 멤버가 도전 중인 미션에 추가
+app.post("/api/v1/member/mission", InsertMemberMissionController);
+
+// 특정 가게의 리뷰를 다 가져오는 api
+app.get("/api/v1/stores/:storeId/reviews", GetListStoreReviewsController);
+
+// 특정 멤버의 리뷰를 다 가져오는 api
+app.get("/api/v1/members/:memberId/reviews", GetListMemberReviewsController);
+
+// 특정 가게의 미션을 가져오는 api
+app.get("/api/v1/stores/:storeId/missions", GetListStoreMissionController);
+
+// 특정 멤버의 진행 중인 미션 목록 가져오는 api
+app.get("/api/v1/members/:memberId/missions", GetListMemberMissionController);
+
+//passport 사용하기
+app.get("/oauth2/login/google", passport.authenticate("google"));
+app.get(
+  "/oauth2/callback/google",
+  passport.authenticate("google", {
+    failureRedirect: "/oauth2/login/google",
+    failureMessage: true,
+  }),
+  (req, res) => res.redirect("/")
+);
 
 app.use(
   "/docs",
@@ -74,52 +159,6 @@ app.get("/openapi.json", async (req, res, next) => {
   res.json(result ? result.data : null);
 });
 
-
-// Case 1. 도메인 문제 일때
-// app.use(
-//   cors({
-//     origin: ["<프론트엔드_주소_및_포트>"],
-//   })
-// );
-
-// Case 2. 토큰 cors 오류 뜰때
-// app.use(
-//   cors({
-//     allowedHeaders: ["x-auth-token", ...],
-//   })
-// );
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-// 회원 가입 api
-app.post("/api/v1/users/signup", handleUserSignUp);
-
-// 가게를 생성하는 api
-app.post("/api/v1/store", InsertStoreController);
-
-// 리뷰를 생성하는 api
-app.post("/api/v1/review", InsertReviewController);
-
-// 가게의 미션 생성하기
-app.post("/api/v1/mission", InsertMissionController);
-
-// 가게의 미션을 멤버가 도전 중인 미션에 추가
-app.post("/api/v1/member/mission", InsertMemberMissionController);
-
-// 특정 가게의 리뷰를 다 가져오는 api
-app.get("/api/v1/stores/:storeId/reviews", GetListStoreReviewsController);
-
-// 특정 멤버의 리뷰를 다 가져오는 api
-app.get("/api/v1/members/:memberId/reviews", GetListMemberReviewsController);
-
-// 특정 가게의 미션을 가져오는 api
-app.get("/api/v1/stores/:storeId/missions", GetListStoreMissionController);
-
-// 특정 멤버의 진행 중인 미션 목록 가져오는 api
-app.get("/api/v1/members/:memberId/missions", GetListMemberMissionController);
-
 /**
  * 전역 오류를 처리하기 위한 미들웨어
  */
@@ -139,3 +178,5 @@ app.use((err, req, res, next) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+app.use(passport.session());
